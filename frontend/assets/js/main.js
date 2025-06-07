5b0c5f4269c189074e8701d95aa927300a8c4fcb
Ali H Shawon <alihshawon@gmail.com>
2025-06-07 14:33:44 +0600

    section-featured

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -16,7 +16,7 @@ async function loadPartial(id, url) {
 // Load all partials on DOMContentLoaded
 document.addEventListener('DOMContentLoaded', () => {
   loadPartial('header', 'partials/header.html');
-  loadPartial('hero', 'partials/hero.html');
+  loadPartial('section-featured', 'partials/section-featured.html');
   loadPartial('section-1', 'partials/section-1.html');
   loadPartial('section-2', 'partials/section-2.html');
   loadPartial('section-3', 'partials/section-3.html');
